3f0f5a87836a2c00003d330e81581b93430ccb35
akorli <alberttamakloe@gmail.com>
2019-04-30 12:28:50 -0500

    Pre-redeploy commit

---
+++ client/build/precache-manifest.e1eaf3541b007f8c17a53ea8e3f62c05.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c3f2769240cb3105293921f08625765",
+    "url": "/static/media/partlycloudyday.9c3f2769.png"
+  },
+  {
+    "revision": "5483dc4b3f1a73f1c51c",
+    "url": "/static/css/main.677cb86e.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "00e3bae5ee27b663ed3a",
+    "url": "/static/js/2.fcc148c4.chunk.js"
+  },
+  {
+    "revision": "540e3938186e4110ae0e242e6ddb10b2",
+    "url": "/static/media/clearday.540e3938.png"
+  },
+  {
+    "revision": "84545c28ebd3cf4593cf57bf95702686",
+    "url": "/static/media/clearnight.84545c28.png"
+  },
+  {
+    "revision": "610765eb644349283d1ed19f30cad11d",
+    "url": "/static/media/fog.610765eb.png"
+  },
+  {
+    "revision": "5483dc4b3f1a73f1c51c",
+    "url": "/static/js/main.2aba5ada.chunk.js"
+  },
+  {
+    "revision": "73c987f2f85ee4a64d35c544b03dd586",
+    "url": "/static/media/partlycloudynight.73c987f2.png"
+  },
+  {
+    "revision": "09b69bcada027526465999bea3c1ba7b",
+    "url": "/static/media/rain.09b69bca.png"
+  },
+  {
+    "revision": "31ce5f2f8d4e8d99c318cdcae1e7f620",
+    "url": "/static/media/sleet.31ce5f2f.png"
+  },
+  {
+    "revision": "4a7491a443a42d4bb9ebdc3d32e6d120",
+    "url": "/static/media/snow.4a7491a4.png"
+  },
+  {
+    "revision": "080ebca57a7ac91a9116380a743b8044",
+    "url": "/static/media/wind.080ebca5.png"
+  },
+  {
+    "revision": "bf4ac9aff36d1636a1b7d8762fe859b8",
+    "url": "/index.html"
+  }
+];
